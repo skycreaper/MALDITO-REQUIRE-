eb2601a89e51b914ef86e7c097b289716f97fd09
juancsr <jcsarmientor@correo.udistrital.edu.co>
2019-07-22 22:40:37 -0500

    Arreglando los errores de mi prro :v

--- Proyecto_final/script.js
+++ Proyecto_final/script.js
@@ -1,20 +1,33 @@
-function scraping() {
-    var c = document.getElementById('dns').value;
-    console.log(c);
-    let cheerio = require('cheerio');
-    let $ = cheerio.load(`
-	<body>
-		<h1>I love jsonframe!</h1>
-		<span itemprop="email"> Email: gabin@datascraper.pro  </span>
-	<body>`)
-    let jsonframe = require('jsonframe-cheerio');
-    jsonframe($); // initializes the plugin
-    let frame = {
-        "title": "h1", // this is an inline selector
-        "email": "span[itemprop=email] < email" // output an extracted email
-    }
-    console.log($('body').scrape(frame, { string: true }))
+var cheerio = require('cheerio');
+var jsonframe = require('jsonframe-cheerio');
+var $ = require('jquery')
+
+$(function() {
+    $('#btnIngresar').click(function() {
+        var c = document.getElementById('dns').value;
+        console.log(c);
+        let $ = cheerio.load(`
+        <body>
+            <h1>I love jsonframe!</h1>
+            <span itemprop="email"> Email: gabin@datascraper.pro  </span>
+        <body>`)
+        jsonframe($); // initializes the plugin
+        let frame = {
+            "title": "h1", // this is an inline selector
+            "email": "span[itemprop=email] < email" // output an extracted email
+        }
+        console.log($('body').scrape(frame, { string: true }))
+    });
+});
+
+
+
+var scraping = function () {
+    // let cheerio = require('cheerio');
+    // let jsonframe = require('jsonframe-cheerio');
 }
+
+
 /*=> output
 {
 	"title": "I love jsonframe!",
